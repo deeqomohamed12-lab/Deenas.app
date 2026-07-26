08ab0a70e3440c9d9c37436be9600189941d37c6
Deeqom2026 <deeqomohamed12@icloud.com>
2026-07-26 16:33:39 -0500

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
-const SUPABASE_URL = "https://dcvrsrghdprzhaciyxir.supabase.co"
+const SUPABASE_URL = "https://dcvrsrghdprzhaciyxir.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-"sb_publishable_oXEymfYSFABZD7cMq9tnnQ_BdMHS1K5"
+"sb_publishable_oXEymfYSFABZD7cMq9tnnQ_BdMHS1K5";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
